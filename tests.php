38c6bab4dff6e8fca40b7f221ead7b5c2156b73d
Norman Koch <norman.koch@tu-dresden.de>
2024-04-28 17:30:08 +0200

    fix

--- tests.php
+++ tests.php
@@ -23,5 +23,12 @@
 
 	print(count($entries_in_db)."\n");
 
+	$replace_first_id = $entries_in_db[0]["_id"]['$oid'];
+
+	$mongodb->replaceDocument($replace_first_id, ["x" => ["y" => 1]]);
+
+	$entries_in_db = $mongodb->find();
+	print(json_encode($entries_in_db));
+
 	print($mongodb->count());
 ?>
